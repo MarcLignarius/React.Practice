c58aaa370b7a2dd8af2561d9b2884be463dcfbe2
Marc Davies <marcdaviesriot@gmail.com>
2020-03-02 10:45:01 -0800

    add deleteTodo method

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -21,7 +21,7 @@ class TodoItem extends Component {
             onChange={this.props.toggleComplete.bind(this, id)}
           />{" "}
           {title}
-          <button style={btnStyle}>x</button>
+          <button onClick={this.props.deleteTodo.bind(this, id)} style={btnStyle}>x</button>
         </p>
       </div>
     );
