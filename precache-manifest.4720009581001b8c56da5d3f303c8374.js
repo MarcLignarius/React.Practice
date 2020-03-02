830df7912310e3542ea5e961b88769b04625b898
Marc Davies <marcdaviesriot@gmail.com>
2020-03-02 14:05:13 -0800

    Updates

---
+++ precache-manifest.4720009581001b8c56da5d3f303c8374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06889a92efdc8cb1f562fee798a6ad10",
+    "url": "/React.Practice/index.html"
+  },
+  {
+    "revision": "55cf488661a1eafd550f",
+    "url": "/React.Practice/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "d964663ebdc59d7ee269",
+    "url": "/React.Practice/static/js/2.51250bd2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React.Practice/static/js/2.51250bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cf488661a1eafd550f",
+    "url": "/React.Practice/static/js/main.07d819df.chunk.js"
+  },
+  {
+    "revision": "1fefa1f2c41478684c72",
+    "url": "/React.Practice/static/js/runtime-main.ab109e3b.js"
+  }
+]);
