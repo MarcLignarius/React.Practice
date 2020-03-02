70f4d71624e1df3ff246567c9a43f83050d8b1a2
Marc Davies <marcdaviesriot@gmail.com>
2020-03-02 10:37:57 -0800

    add button to delete items and add styles

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -21,6 +21,7 @@ class TodoItem extends Component {
             onChange={this.props.toggleComplete.bind(this, id)}
           />{" "}
           {title}
+          <button style={btnStyle}>x</button>
         </p>
       </div>
     );
@@ -29,6 +30,16 @@ class TodoItem extends Component {
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
 };
 
 export default TodoItem;
