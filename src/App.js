b05d658e0a411ab6b4d7d9bba5cc037ba9aaaa86
Marc Davies <marcdaviesriot@gmail.com>
2020-03-02 10:24:23 -0800

    create markComplete method and pass id from Todoitem to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Todos from './components/Todos';
-import './App.css';
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -19,14 +19,18 @@ class App extends Component {
         id: 3,
         title: "meeting with boss",
         completed: false
-      },
+      }
     ]
-  }
-  
+  };
+
+  markComplete = (id) => {
+    console.log(id);
+  };
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
